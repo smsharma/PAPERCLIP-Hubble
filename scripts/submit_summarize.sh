8260ecab5df5c6dc0cf91957e72ef10065b749f1
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-12-31 22:20:24 -0500

    Submit summarize with mixtral

--- scripts/submit_summarize.sh
+++ scripts/submit_summarize.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=summarize
+#SBATCH --job-name=summarize_mixtral
 #SBATCH --nodes=1
 #SBATCH --mem=80GB
 #SBATCH --time=48:00:00
 #SBATCH --gres=gpu:4
 #SBATCH --account=iaifi_lab
-#SBATCH -p iaifi_gpu
+#SBATCH -p iaifi_gpu_priority
 
 export TF_CPP_MIN_LOG_LEVEL="2"
 
@@ -26,4 +26,4 @@ alias python=/n/holystore01/LABS/iaifi_lab/Users/smsharma/envs/$ENV/bin/python
 alias jupyter=/n/holystore01/LABS/iaifi_lab/Users/smsharma/envs/$ENV/bin/jupyter
 
 cd /n/holystore01/LABS/iaifi_lab/Users/smsharma/multimodal-data/
-/n/holystore01/LABS/iaifi_lab/Users/smsharma/envs/$ENV/bin/python summarize.py --data_dir /n/holystore01/LABS/iaifi_lab/Users/smsharma/multimodal-data/data --observations_dir observations_v1 --save_filename summary_v1
+/n/holystore01/LABS/iaifi_lab/Users/smsharma/envs/$ENV/bin/python summarize.py --data_dir /n/holystore01/LABS/iaifi_lab/Users/smsharma/multimodal-data/data --observations_dir observations_v1 --save_filename summary_v2 --verbose
