59f7f803c9588bd896e1095f76aa73562451ef53
Siddharth Mishra-Sharma <smsharma@mit.edu>
2024-03-03 14:21:15 -0500

    Plot updates

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -3,16 +3,21 @@ import jax
 import pandas as pd
 
 
-def process_truncate_captions(captions, key, max_length_words=None, use_sum1=False, df_sum_merged=None):
-    """ Process and truncate captions
-    """
+def process_truncate_captions(
+    captions, key, max_length_words=None, use_sum1=False, df_sum_merged=None
+):
+    """Process and truncate captions"""
     captions = captions.numpy().tolist()
     captions = [c.decode("utf-8") for c in captions]
 
     def get_objects_phenomena(caption):
-        first_part = caption.split(';')[0]
-        match = df_sum_merged[df_sum_merged['objects_phenomena_x'] == first_part]['objects_phenomena_y']
-        return match.values[0] if not (match.empty or pd.isna(match.values[0])) else "None"
+        first_part = caption.split(";")[0]
+        match = df_sum_merged[df_sum_merged["objects_phenomena_x"] == first_part][
+            "objects_phenomena_y"
+        ]
+        return (
+            match.values[0] if not (match.empty or pd.isna(match.values[0])) else "None"
+        )
 
     # Get sum1 summaries by matching
     if use_sum1 and df_sum_merged is not None:
@@ -25,22 +30,30 @@ def process_truncate_captions(captions, key, max_length_words=None, use_sum1=Fal
     return captions
 
 
-def tokenize_captions(captions, tokenizer, max_length_tokens, max_length_words=None, key=None):
-    """ Tokenizer if using custom CLIP model
-    """
-    captions = process_truncate_captions(captions, key, max_length_words=max_length_words)
-    captions = tokenizer(captions, padding="max_length", truncation=True, max_length=max_length_tokens, return_tensors="np")
+def tokenize_captions(
+    captions, tokenizer, max_length_tokens, max_length_words=None, key=None
+):
+    """Tokenizer if using custom CLIP model"""
+    captions = process_truncate_captions(
+        captions, key, max_length_words=max_length_words
+    )
+    captions = tokenizer(
+        captions,
+        padding="max_length",
+        truncation=True,
+        max_length=max_length_tokens,
+        return_tensors="np",
+    )
     return captions["input_ids"], captions["attention_mask"]
 
 
 def sample_and_pad(captions, key, max_length=77):
-    """ Sample a random sequence of sentences up to max_length in length
-    """
+    """Sample a random sequence of sentences up to max_length in length"""
     sampled_seqs = []
 
     for caption in captions:
         sentences = nltk.sent_tokenize(caption)
-        
+
         # If the total words are already less than or equal to max_length, just append the caption
         if len(caption.split()) <= max_length:
             sampled_seqs.append(caption)
@@ -48,7 +61,9 @@ def sample_and_pad(captions, key, max_length=77):
 
         # Get random start index for sentences
         key, subkey = jax.random.split(key)
-        start_idx = jax.random.randint(subkey, minval=0, maxval=len(sentences), shape=())
+        start_idx = jax.random.randint(
+            subkey, minval=0, maxval=len(sentences), shape=()
+        )
 
         # Rotate the sentences so that the starting sentence is at the beginning
         rotated_sentences = sentences[start_idx:] + sentences[:start_idx]
